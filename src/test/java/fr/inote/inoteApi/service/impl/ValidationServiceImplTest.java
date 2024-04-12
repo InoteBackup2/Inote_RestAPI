d03c567000f2d3870607452ecb0abc199bb2b3f1
atsuhikoMochizuki <atsuhiko_mochizuki27@laposte.net>
2024-04-12 12:06:57 +0200

    ValidationServiceImplTest passed

--- src/test/java/fr/inote/inoteApi/service/impl/ValidationServiceImplTest.java
+++ src/test/java/fr/inote/inoteApi/service/impl/ValidationServiceImplTest.java
@@ -7,9 +7,7 @@ import fr.inote.inoteApi.entity.Role;
 import fr.inote.inoteApi.entity.User;
 import fr.inote.inoteApi.entity.Validation;
 import fr.inote.inoteApi.repository.ValidationRepository;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -24,65 +22,83 @@ import java.util.Optional;
 import static fr.inote.inoteApi.ConstantsForTests.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.catchThrowable;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 /**
- * The type Validation service impl test.
+ * Unit tests of service ValidationServiceImplTest
  *
  * @author atsuhiko Mochizuki
- * @date 27/03/2024
+ * @date 28/03/2024
+ */
+
+/*
+ * The @ActiveProfiles annotation in Spring is used to declare which active bean
+ * definition profiles
+ * should be used when loading an ApplicationContext for test classes
  */
 @ActiveProfiles("test")
-@Tag("Service tests")
+
+/* Add Mockito functionalities to Junit 5 */
 @ExtendWith(MockitoExtension.class)
 class ValidationServiceImplTest {
 
-    /* Dependencies and injection*/
+    /* DEPENDENCIES MOCKING */
+    /* ============================================================ */
+    /* @Mock create and inject mocked instances of classes */
     @Mock
     private ValidationRepository mockedValidationRepository;
     @Mock
     private NotificationServiceImpl mockedNotificationService;
 
+    /* DEPENDENCIES INJECTION */
+    /* ============================================================ */
+    /*
+     * @InjectMocks instance class to be tested and automatically inject mock fields
+     * Nota : if service is an interface, instanciate implementation withs mocks in
+     * params
+     */
     @InjectMocks
     private ValidationServiceImpl validationService;
 
-    /* attributes*/
-    private Validation validationRef;
+    /* REFERENCES FOR MOCKING */
+    /* ============================================================ */
+    private Role roleForTest = Role.builder().name(RoleEnum.ADMIN).build();
 
-    private User userForTest;
+    private User userForTest = User.builder()
+            .email(REFERENCE_USER_EMAIL)
+            .name(REFERENCE_USER_NAME)
+            .password(REFERENCE_USER_PASSWORD)
+            .role(roleForTest)
+            .build();
 
-    @BeforeEach
-    public void init() {
-        Role roleForTest = Role.builder().name(RoleEnum.ADMIN).build();
+    private Validation validationRef = Validation.builder()
+            .code("123456")
+            .user(this.userForTest)
+            .creation(Instant.now())
+            .expiration(Instant.now().plus(5, ChronoUnit.MINUTES))
+            .build();
 
-        this.userForTest = User.builder()
-                .email(REFERENCE_USER_EMAIL)
-                .name(REFERENCE_USER_NAME)
-                .password(REFERENCE_USER_PASSWORD)
-                .role(roleForTest)
-                .build();
+    /* FIXTURES */
+    /* ============================================================ */
+    // @BeforeEach
+    // void setUp() {}
 
-        this.validationRef = Validation.builder()
-                .code("123456")
-                .user(this.userForTest)
-                .creation(Instant.now())
-                .expiration(Instant.now().plus(5, ChronoUnit.MINUTES))
-                .build();
-    }
-
+    /* SERVICE UNIT TESTS */
+    /* ============================================================ */
     @Test
     @DisplayName("Create validation ans save when user not exist in db and data are correct")
-    void createAndSave_shouldCreateAnewValidationInDbAndSendCodeActivation_whenUserIsCorrectAndNotExistsInDatabase() throws InoteInvalidEmailException {
-        // ARRANGE
+    void createAndSave_shouldCreateAnewValidationInDbAndSendCodeActivation_whenUserIsCorrectAndNotExistsInDatabase()
+            throws InoteInvalidEmailException {
+        /* Arrange */
         when(this.mockedValidationRepository.save(any(Validation.class))).thenReturn(this.validationRef);
         doNothing().when(this.mockedNotificationService).sendValidation_byEmail(any(Validation.class));
 
-
-        // ACT
+        /* Act */
         Instant instantCreation = Instant.now();
         Validation validationTest = this.validationService.createAndSave(this.userForTest);
 
-        // ASSERT
+        /* Assert */
         assertThat(validationTest).isNotNull();
         assertThat(validationTest).isInstanceOf(Validation.class);
 
@@ -97,32 +113,43 @@ class ValidationServiceImplTest {
         assertThat(validationTest.getActivation()).isNull();
 
         assertThat(validationTest.getExpiration()).isAfter(validationTest.getCreation().plusMillis(50));
-        assertThat(validationTest.getExpiration()).isAfter(validationTest.getCreation().plus(5, ChronoUnit.MINUTES).minusMillis(50));
+        assertThat(validationTest.getExpiration())
+                .isAfter(validationTest.getCreation().plus(5, ChronoUnit.MINUTES).minusMillis(50));
 
+        /* Verify */
         verify(this.mockedValidationRepository, times(1)).save(any(Validation.class));
         verify(this.mockedNotificationService, times(1)).sendValidation_byEmail(any(Validation.class));
     }
 
-
     @Test
     @DisplayName("Search by activation code an existing validation in database")
     void getValidationFromCode_ShouldSuccess_WhenValidationExists() throws InoteValidationNotFoundException {
+        /* Arrange */
         when(this.mockedValidationRepository.findByCode(any(String.class))).thenReturn(Optional.of(this.validationRef));
 
-        assertThat(this.validationService.getValidationFromCode(this.validationRef.getCode())).isEqualTo(this.validationRef);
+        /* Act & assert */
+        assertThat(this.validationService.getValidationFromCode(this.validationRef.getCode()))
+                .isEqualTo(this.validationRef);
 
+        /* Verify */
         verify(this.mockedValidationRepository, times(1)).findByCode(any(String.class));
     }
 
     @Test
     @DisplayName("Search by activation code an non- existing validation in database")
     void getValidationFromCode_ShouldFail_WhenValidationNotExists() {
+
+        /* Arrange */
         when(this.mockedValidationRepository.findByCode(any(String.class))).thenReturn(Optional.empty());
 
-        Throwable thrown = catchThrowable(() -> this.validationService.getValidationFromCode(this.validationRef.getCode()));
+        /* Act */
+        Throwable thrown = catchThrowable(
+                () -> this.validationService.getValidationFromCode(this.validationRef.getCode()));
 
+        /* Assert */
         assertThat(thrown).isInstanceOf(InoteValidationNotFoundException.class);
 
+        /* Verify */
         verify(this.mockedValidationRepository, times(1)).findByCode(any(String.class));
     }
 
