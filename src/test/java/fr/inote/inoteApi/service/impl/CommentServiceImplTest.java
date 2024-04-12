878865b3824e57efe03153e451c720454abba595
atsuhikoMochizuki <atsuhiko_mochizuki27@laposte.net>
2024-04-12 11:23:25 +0200

    CommentServiceImplTest all tests pass

--- src/test/java/fr/inote/inoteApi/service/impl/CommentServiceImplTest.java
+++ src/test/java/fr/inote/inoteApi/service/impl/CommentServiceImplTest.java
@@ -2,8 +2,6 @@ package fr.inote.inoteApi.service.impl;
 
 import fr.inote.inoteApi.crossCutting.enums.RoleEnum;
 import fr.inote.inoteApi.crossCutting.exceptions.InoteEmptyMessageCommentException;
-import fr.inote.inoteApi.crossCutting.security.Jwt;
-import fr.inote.inoteApi.crossCutting.security.RefreshToken;
 import fr.inote.inoteApi.dto.CommentDtoRequest;
 import fr.inote.inoteApi.entity.Comment;
 import fr.inote.inoteApi.entity.Role;
@@ -11,7 +9,6 @@ import fr.inote.inoteApi.entity.User;
 import fr.inote.inoteApi.repository.CommentRepository;
 import fr.inote.inoteApi.repository.UserRepository;
 import fr.inote.inoteApi.service.CommentService;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -23,89 +20,95 @@ import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.test.context.ActiveProfiles;
 
-import java.time.Instant;
-import java.time.temporal.ChronoUnit;
 import java.util.Optional;
 
 import static fr.inote.inoteApi.ConstantsForTests.*;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static org.assertj.core.api.Assertions.*;
 
+/**
+ * Unit tests of service CommentService
+ *
+ * @author atsuhiko Mochizuki
+ * @date 28/03/2024
+ */
+
+/*
+ * The @ActiveProfiles annotation in Spring is used to declare which active bean
+ * definition profiles
+ * should be used when loading an ApplicationContext for test classes
+ */
 @ActiveProfiles("test")
+
+/* Add Mockito functionalities to Junit 5 */
 @ExtendWith(MockitoExtension.class)
 class CommentServiceImplTest {
+
+        /* DEPENDENCIES MOCKING */
+        /* ============================================================ */
+        /* @Mock create and inject mocked instances of classes */
         @Mock
         private CommentRepository commentRepository;
         @Mock
         private UserRepository userRepository;
+
+        /* DEPENDENCIES INJECTION */
+        /* ============================================================ */
+        /*
+         * @InjectMocks instance class to be tested and automatically inject mock fields
+         * Nota : if service is an interface, instanciate implementation withs mocks in
+         * params
+         */
         @InjectMocks
         private CommentService commentService = new CommentServiceImpl(commentRepository, userRepository);
 
-        private Jwt jwtRef;
-        private User userRef;
-        private RefreshToken refreshToken;
-        private Comment commentRef;
+        /* REFERENCES FOR MOCKING */
+        /* ============================================================ */
+        Role roleForTest = Role.builder().name(RoleEnum.ADMIN).build();
 
-        private CommentDtoRequest commentDtoRequestRef;
+        private User userRef = User.builder()
+                        .email(REFERENCE_USER_EMAIL)
+                        .name(REFERENCE_USER_NAME)
+                        .password(REFERENCE_USER_PASSWORD)
+                        .role(roleForTest)
+                        .build();
 
-        @BeforeEach
-        void setUp() {
+        private CommentDtoRequest commentDtoRequestRef = new CommentDtoRequest(
+                        "Application should provide most functionalities");
 
-                // Reference creation
-                Role roleForTest = Role.builder().name(RoleEnum.ADMIN).build();
-                this.userRef = User.builder()
-                                .email(REFERENCE_USER_EMAIL)
-                                .name(REFERENCE_USER_NAME)
-                                .password(REFERENCE_USER_PASSWORD)
-                                .role(roleForTest)
-                                .build();
+        private Comment commentRef = Comment.builder()
+                        .message(this.commentDtoRequestRef.msg())
+                        .user(this.userRef)
+                        .build();
 
-                this.refreshToken = RefreshToken.builder()
-                                .expirationStatus(false)
-                                .contentValue("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiU2FuZ29rdSIsImV4cCI6MTg2OTY3NTk5Niwic3ViIjoic2FuZ29rdUBpbm90ZS5mciJ9.ni8Z4Wiyo6-noIme2ydnP1vHl6joC0NkfQ-lxF501vY")
-                                .creationDate(Instant.now())
-                                .expirationDate(Instant.now().plus(10, ChronoUnit.MINUTES))
-                                .build();
+        /* FIXTURES */
+        /* ============================================================ */
+        // @BeforeEach
+        // void setUp() {}
 
-                jwtRef = Jwt.builder()
-                                .id(1)
-                                .user(this.userRef)
-                                .contentValue("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c")
-                                .deactivated(false)
-                                .expired(false)
-                                .refreshToken(this.refreshToken)
-                                .build();
-
-                this.commentDtoRequestRef = new CommentDtoRequest("Application should provide most functionalities");
-
-                this.commentRef = Comment.builder()
-                                .message(this.commentDtoRequestRef.msg())
-                                .user(this.userRef)
-                                .build();
-        }
-
+        /* SERVICE UNIT TESTS */
+        /* ============================================================ */
         @Test
         @DisplayName("Create comment when user is connected")
         void createComment_ShouldSuccess_whenUserIsConnected() throws InoteEmptyMessageCommentException {
-                // Arrange
+
+                /* Arrange */
                 Authentication auth = mock(Authentication.class);
                 when(auth.getPrincipal()).thenReturn(this.userRef);
-
                 SecurityContext securityContext = mock(SecurityContext.class);
                 when(securityContext.getAuthentication()).thenReturn(auth);
                 SecurityContextHolder.setContext(securityContext);
+
                 when(this.commentRepository.save(any(Comment.class))).thenReturn(this.commentRef);
                 when(this.userRepository.findByEmail(anyString())).thenReturn(Optional.of(this.userRef));
 
-                // Act & assert
+                /* Act */
                 Comment commentForTest = this.commentService.createComment(this.commentDtoRequestRef.msg());
-                // assertThat(commentForTest.getUser()).isEqualTo(this.userRef);
-                // Voir Ici! Pourquoi il ne retourne pas le user lors de la sauvegarde?
-                // fail();
+
+                /* Assert */
                 assertThat(commentForTest.getMessage()).isEqualTo(this.commentDtoRequestRef.msg());
 
         }
@@ -113,13 +116,13 @@ class CommentServiceImplTest {
         @Test
         @DisplayName("Create comment when user is not connected")
         void createComment_ShouldFail_whenUserIsNotConnected() {
-                // Arrange
-                Authentication auth = mock(Authentication.class);
+                /* Arrange */
+                // Authentication auth = mock(Authentication.class);
                 SecurityContext securityContext = mock(SecurityContext.class);
                 when(securityContext.getAuthentication()).thenReturn(null);
                 SecurityContextHolder.setContext(securityContext);
 
-                // Act & assert
+                /* Act & assert */
                 assertThatExceptionOfType(NullPointerException.class)
                                 .isThrownBy(() -> this.commentService.createComment(this.commentDtoRequestRef.msg()));
         }
@@ -128,15 +131,15 @@ class CommentServiceImplTest {
         @DisplayName("Create comment when user is not connected message is empty or blank")
         void createComment_ShouldFail_whenUserIsConnectedAndMessageIsEmptyOrBlank()
                         throws InoteEmptyMessageCommentException {
-                // Arrange
+
+                /* Arrange */
                 Authentication auth = mock(Authentication.class);
                 when(auth.getPrincipal()).thenReturn(this.userRef);
-
                 SecurityContext securityContext = mock(SecurityContext.class);
                 when(securityContext.getAuthentication()).thenReturn(auth);
                 SecurityContextHolder.setContext(securityContext);
 
-                // Act & assert
+                /* Act & assert */
                 String commentDto1 = new String("");
                 assertThatExceptionOfType(InoteEmptyMessageCommentException.class)
                                 .isThrownBy(() -> this.commentService.createComment(commentDto1));
