38bc13f30c55601c6b5199d12cd6edc76f331124
atsuhikoMochizuki <atsuhiko_mochizuki27@laposte.net>
2024-04-12 11:41:48 +0200

    NotificationServiceImplTest passed

--- src/test/java/fr/inote/inoteApi/service/impl/NotificationServiceImplTest.java
+++ src/test/java/fr/inote/inoteApi/service/impl/NotificationServiceImplTest.java
@@ -22,10 +22,8 @@ import fr.inote.inoteApi.entity.User;
 import fr.inote.inoteApi.entity.Validation;
 import javax.mail.internet.MimeMessage;
 
-import lombok.NonNull;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.extension.RegisterExtension;
@@ -37,64 +35,95 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.test.context.ActiveProfiles;
 
+/*
+ * The @ActiveProfiles annotation in Spring is used to declare which active bean
+ * definition profiles
+ * should be used when loading an ApplicationContext for test classes
+ */
 @ActiveProfiles("test")
-@Tag("Service tests")
+
+/* Add Mockito functionalities to Junit 5 */
 @ExtendWith(MockitoExtension.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+
+/* Load entire context app */
+@SpringBootTest
 public class NotificationServiceImplTest {
 
+        /* JUNIT EXTENSIONS */
+        /* ============================================================ */
         @RegisterExtension
-        public
-        static final GreenMailExtension greenMail = new GreenMailExtension(ServerSetupTest.SMTP)
+        public static final GreenMailExtension greenMail = new GreenMailExtension(ServerSetupTest.SMTP)
                         .withConfiguration(GreenMailConfiguration.aConfig().withUser("duke", "springboot"))
                         .withPerMethodLifecycle(false);
 
+        /* DEPENDENCIES MOCKING */
+        /* ============================================================ */
+        /* @Mock create and inject mocked instances of classes */
         /* Dependencies */
-        final JavaMailSender javaMailSender;
-
         @Mock
         private NotificationServiceImpl mockedNotificationService;
 
+        /* DEPENDENCIES INJECTION */
+        /* ============================================================ */
+        /*
+         * @InjectMocks instance class to be tested and automatically inject mock fields
+         * Nota : if service is an interface, instanciate implementation withs mocks in
+         * params
+         */
         @InjectMocks
         private NotificationServiceImpl notificationService;
 
+        final JavaMailSender javaMailSender;
+
+        // Constructor injection
         @Autowired
         public NotificationServiceImplTest(NotificationServiceImpl notificationService, JavaMailSender javaMailSender) {
                 this.notificationService = notificationService;
                 this.javaMailSender = javaMailSender;
         }
 
-        /* Dependencies injections */
+        /* CONSTANTS */
+        /* ============================================================ */
         public final String SENDER_EMAIL = "sangoku@kame-house.jp";
         public final String RECEIVER_EMAIL = "kaio@kaio.uni";
         public final String SUBJECT_OF_EMAIL = "Well arrived";
         public final String BODY_OF_EMAIL = "We are arrived";
 
+        /* FIXTURES */
+        /* ============================================================ */
         @BeforeEach
-        void setUp() throws FolderException{
+        void setUp() throws FolderException {
                 NotificationServiceImplTest.greenMail.purgeEmailFromAllMailboxes();
         }
 
+        /* SERVICE UNIT TESTS */
+        /* ============================================================ */
         @Test
         @DisplayName("Sending email with good parameters")
         public void sendEmail_ShouldSuccess_withGoodParams() throws NoSuchMethodException, SecurityException {
 
-                /* Access to private method with java reflexion */
+                /* Arrange */
+                // Access to private method with java reflexion
                 Method privateMethod_sendEmail = getPrivateMethodSendEmail();
 
+                /* Act & assert */
                 assertThatCode(() -> privateMethod_sendEmail.invoke(this.notificationService,
-                                SENDER_EMAIL, RECEIVER_EMAIL, SUBJECT_OF_EMAIL, BODY_OF_EMAIL)).doesNotThrowAnyException();
+                                SENDER_EMAIL, RECEIVER_EMAIL, SUBJECT_OF_EMAIL, BODY_OF_EMAIL))
+                                .doesNotThrowAnyException();
         }
 
         @Test
         @DisplayName("Mocking smtp server with GreenMail")
         void test_shouldSuccess_WhenMockingSmtpServerWithGreenmail() throws NoSuchMethodException, SecurityException {
 
-                /* Access to private method with java reflexion */
+                /* Arrange */
+                // Access to private method with java reflexion
                 Method privateMethod_sendEmail = getPrivateMethodSendEmail();
 
+                /* Act & assert */
                 assertThatCode(() -> privateMethod_sendEmail.invoke(this.notificationService,
-                                SENDER_EMAIL, RECEIVER_EMAIL, SUBJECT_OF_EMAIL, BODY_OF_EMAIL)).doesNotThrowAnyException();
+                                SENDER_EMAIL, RECEIVER_EMAIL, SUBJECT_OF_EMAIL, BODY_OF_EMAIL))
+                                .doesNotThrowAnyException();
 
                 await()
                                 .atMost(2, SECONDS)
@@ -111,26 +140,15 @@ public class NotificationServiceImplTest {
                                 });
         }
 
-        @NonNull
-        private static Method getPrivateMethodSendEmail() throws NoSuchMethodException {
-                Method privateMethod_sendEmail = NotificationServiceImpl.class
-                                .getDeclaredMethod("sendEmail",
-                                                String.class,
-                                                String.class,
-                                                String.class,
-                                                String.class);
-
-                privateMethod_sendEmail.setAccessible(true);
-                return privateMethod_sendEmail;
-        }
-
         @Test
         @DisplayName("Sending email with malFormated address")
         public void sendEmail_ShouldFail_withMalFormatedAddress() throws NoSuchMethodException, SecurityException,
-                IllegalArgumentException {
+                        IllegalArgumentException {
 
-                /* Access to private method with java reflexion */
+                /* Arrange */
                 Method privateMethod_sendEmail = getPrivateMethodSendEmail();
+
+                /* Act & assert */
 
                 // with 2 @
                 Throwable thrown = catchThrowable(() -> privateMethod_sendEmail.invoke(this.notificationService,
@@ -138,6 +156,7 @@ public class NotificationServiceImplTest {
                                 RECEIVER_EMAIL,
                                 SUBJECT_OF_EMAIL,
                                 BODY_OF_EMAIL));
+
                 assertThat(thrown)
                                 .isInstanceOf(InvocationTargetException.class)
                                 .hasCauseInstanceOf(InoteInvalidEmailException.class);
@@ -167,6 +186,7 @@ public class NotificationServiceImplTest {
         @DisplayName("Sending activation code by email with good informations")
         public void sendValidation_byEmail_shouldSuccess_whenParametersAreGood() {
 
+                /* Arrange */
                 User userTest = User.builder()
                                 .email(RECEIVER_EMAIL)
                                 .name("Sangoku")
@@ -181,6 +201,7 @@ public class NotificationServiceImplTest {
                                 .user(userTest)
                                 .build();
 
+                /* Act & assert */
                 assertThatCode(() -> this.notificationService.sendValidation_byEmail(validationTest))
                                 .doesNotThrowAnyException();
 
@@ -203,7 +224,7 @@ public class NotificationServiceImplTest {
         @Test
         @DisplayName("Sending activation code by email with bad parameters")
         public void sendValidation_byEmail_shouldFail_whenParametersAreBad() {
-
+                /* Arrange */
                 User userTest = User.builder()
                                 .email("sangoku @ inote.fr")
                                 .name("Sangoku")
@@ -217,10 +238,26 @@ public class NotificationServiceImplTest {
                                 .expiration(Instant.now().plus(10, ChronoUnit.MINUTES))
                                 .user(userTest)
                                 .build();
+                /* Act */
+                Throwable thrown = catchThrowable(
+                                () -> this.notificationService.sendValidation_byEmail(validationTest));
 
-                Throwable thrown = catchThrowable(() -> this.notificationService.sendValidation_byEmail(validationTest));
-
+                /* Assert */
                 assertThat(thrown).isInstanceOf(InoteInvalidEmailException.class);
         }
 
+        /* UTILS */
+        /* ============================================================ */
+        private static Method getPrivateMethodSendEmail() throws NoSuchMethodException {
+
+                Method privateMethod_sendEmail = NotificationServiceImpl.class
+                                .getDeclaredMethod("sendEmail",
+                                                String.class,
+                                                String.class,
+                                                String.class,
+                                                String.class);
+
+                privateMethod_sendEmail.setAccessible(true);
+                return privateMethod_sendEmail;
+        }
 }
