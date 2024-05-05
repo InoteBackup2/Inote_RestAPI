1076686e221071827c5bdd1b491e3ab027b4f95c
Atsuhiko Mochizuki <129963944+atsuhikoMochizuki@users.noreply.github.com>
2024-05-06 00:19:21 +0200

    update CORS policy

--- src/main/java/fr/inote/inoteApi/crossCutting/security/WebConfig.java
+++ src/main/java/fr/inote/inoteApi/crossCutting/security/WebConfig.java
@@ -12,6 +12,7 @@ public class WebConfig {
     @Bean
 	public WebMvcConfigurer corsConfigurer() {
 		return new WebMvcConfigurer() {
+			@Value("${server.name}")
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping(Endpoint.REGISTER).allowedOrigins("http://localhost:4200");
