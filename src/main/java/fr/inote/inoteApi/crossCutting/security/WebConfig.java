be12786000c8b65388a08b9b10fc7d5e5aa4cf3c
Atsuhiko Mochizuki <129963944+atsuhikoMochizuki@users.noreply.github.com>
2024-05-10 12:59:00 +0200

    Update -- 2024-05-10_12:59:00

--- src/main/java/fr/inote/inoteApi/crossCutting/security/WebConfig.java
+++ src/main/java/fr/inote/inoteApi/crossCutting/security/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping(Endpoint.REGISTER)
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins("http://localhost:9000")
                 .allowedMethods("POST")
                 // .allowedHeaders("Content-Type", "Authorization")
                 .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials");
