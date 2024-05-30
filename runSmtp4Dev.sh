5c45264a8b016d4483e8f8f2c330027de827bc0d
Frédéric Y. Henry <129835380+FYHenry@users.noreply.github.com>
2024-05-30 18:04:36 +0200

    runSmtp4Dev.sh updated
    Le *shebang* ajouté.
    
    Un peu de factorisation.
    
    Je ne sais pas si c’est *POSIX-compliant*.

--- runSmtp4Dev.sh
+++ runSmtp4Dev.sh
@@ -1,6 +1,9 @@
+#/usr/bin/bash
 # Run SMTP4DEV
 # AtsuhikoMochizuki 27/05/2024
 #===================================================================================
+readonly TITLE='SMTP4Dev'
+readonly BIN_PATH='../../smtp-dev/Rnwood.Smtp4dev'
 clear
 echo "
   _____ _   _  ____ _______ ______ 
@@ -17,15 +20,16 @@ echo "
  ==============================================================================                                 
 "
 echo -e -n "\033[93m -- Search Smtp4dev (smtp server mocking)...\033[0m"
-if [ -f ../../smtp-dev/Rnwood.Smtp4dev ]; then
+if [ -f "$BIN_PATH" ]
+then
     echo -e "\033[32mOK\033[0m"
-    
     echo -e "\033[93m -- Launch Smtp-dev in new terminal\033[0m"
-    gnome-terminal --title=SMTP4DEV --command "../../smtp-dev/Rnwood.Smtp4dev"
-    xdotool windowminimize $(xdotool search --name SMTP4DEV|head -1)
+    gnome-terminal --title="$TITLE" --command "$BIN_PATH"
+    xdotool windowminimize $(xdotool search --name "$TITLE"|head -1)
     echo -e -n "\033[93m -- \033[0m"
     echo -e "\033[32mOK\033[0m"
 else
-    echo -e "\033[31mNot Found : This script expects to find a ‘smtp-dev’ folder in the folder containing the super project folder, which contains the Smtp4dev simulation smtp server. Please download Smtp4Dev and install it in the same folder as the Inote root superproject.\033[0m"
+    echo -e "\033[31mNot Found : This script expects to find a ‘smtp-dev’ directory in the folder containing the superproject directory,"\
+    " which contains the Smtp4dev simulation smtp server."\
+    echo -e " Please download Smtp4Dev and install it in the same folder as the Inote root superproject.\033[0m"
 fi
-
