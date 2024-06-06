94131738bd7def80969933d34344b9238e0eb7fd
FYHenry <frederic_yann.henry@protonmail.com>
2024-06-06 22:00:25 +0200

    Added configuration to localize
    Texte traduit selon ce qui sera indiqué dans l’en-tête HTTP `Accept-Language`.

---
+++ src/main/java/fr/inote/inote_api/cross_cutting/security/LocaleConfig.java
@@ -0,0 +1,26 @@
+package fr.inote.inote_api.cross_cutting.security;
+
+import java.util.Locale;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+
+@Configuration
+public class LocaleConfig {
+    @Bean
+    public AcceptHeaderLocaleResolver getLocaleResolver() {
+        final AcceptHeaderLocaleResolver acceptHeaderLocaleResolver = new AcceptHeaderLocaleResolver();
+        acceptHeaderLocaleResolver.setDefaultLocale(Locale.ENGLISH);
+        return acceptHeaderLocaleResolver;
+    }
+
+    @Bean
+    public ResourceBundleMessageSource getMessageSource() {
+        final ResourceBundleMessageSource resourceBundleMessageSource = new ResourceBundleMessageSource();
+        resourceBundleMessageSource.setBasename("i18n/messages");
+        resourceBundleMessageSource.setDefaultEncoding("UTF-8");
+        return resourceBundleMessageSource;
+    }
+}
